6c4312bbc6f17795f2529a2c7cf9b632ad395b11
Mohamed Fakhry <mohamedfakhry58890@gmail.com>
2017-08-21 01:30:45 +0200

    Init Project Structure

---
+++ app/src/main/java/com/example/computec/eltadreb/ui/base/BasePresenter.java
@@ -0,0 +1,103 @@
+package com.example.computec.eltadreb.ui.base;
+
+import android.util.Log;
+
+import com.androidnetworking.common.ANConstants;
+import com.androidnetworking.error.ANError;
+import com.example.computec.eltadreb.R;
+import com.example.computec.eltadreb.data.network.model.ApiError;
+import com.example.computec.eltadreb.utils.AppConstants;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class BasePresenter<V extends MvpView> implements MvpPresenter<V> {
+
+    private static final String TAG = "BasePresenter";
+
+
+    private V mMvpView;
+
+    @Override
+    public void onAttach(V mvpView) {
+        mMvpView = mvpView;
+    }
+
+    @Override
+    public void onDetach() {
+        mMvpView = null;
+    }
+
+    public boolean isViewAttached() {
+        return mMvpView != null;
+    }
+
+    public V getMvpView() {
+        return mMvpView;
+    }
+
+    public void checkViewAttached() {
+        if (!isViewAttached()) throw new MvpViewNotAttachedException();
+    }
+
+    @Override
+    public void handleApiError(ANError error) {
+
+        if (error == null || error.getErrorBody() == null) {
+            getMvpView().onError(R.string.api_default_error);
+            return;
+        }
+
+        if (error.getErrorCode() == AppConstants.API_STATUS_CODE_LOCAL_ERROR
+                && error.getErrorDetail().equals(ANConstants.CONNECTION_ERROR)) {
+            getMvpView().onError(R.string.connection_error);
+            return;
+        }
+
+        if (error.getErrorCode() == AppConstants.API_STATUS_CODE_LOCAL_ERROR
+                && error.getErrorDetail().equals(ANConstants.REQUEST_CANCELLED_ERROR)) {
+            getMvpView().onError(R.string.api_retry_error);
+            return;
+        }
+
+        final GsonBuilder builder = new GsonBuilder().excludeFieldsWithoutExposeAnnotation();
+        final Gson gson = builder.create();
+
+        try {
+            ApiError apiError = gson.fromJson(error.getErrorBody(), ApiError.class);
+
+            if (apiError == null || apiError.getMessage() == null) {
+                getMvpView().onError(R.string.api_default_error);
+                return;
+            }
+
+            switch (error.getErrorCode()) {
+                case HttpsURLConnection.HTTP_UNAUTHORIZED:
+                case HttpsURLConnection.HTTP_FORBIDDEN:
+                    setUserAsLoggedOut();
+                    getMvpView().openActivityOnTokenExpire();
+                case HttpsURLConnection.HTTP_INTERNAL_ERROR:
+                case HttpsURLConnection.HTTP_NOT_FOUND:
+                default:
+                    getMvpView().onError(apiError.getMessage());
+            }
+        } catch (JsonSyntaxException | NullPointerException e) {
+            Log.e(TAG, "handleApiError", e);
+            getMvpView().onError(R.string.api_default_error);
+        }
+    }
+
+    @Override
+    public void setUserAsLoggedOut() {
+
+    }
+
+    public static class MvpViewNotAttachedException extends RuntimeException {
+        public MvpViewNotAttachedException() {
+            super("Please call Presenter.onAttach(MvpView) before" +
+                    " requesting data to the Presenter");
+        }
+    }
+}
