da3708255216b38187a082c5b8f55df34a831a44
Mohamed Fakhry <mohamedfakhry58890@gmail.com>
2017-08-23 22:07:04 +0200

    Add font

--- app/src/main/java/com/example/computec/eltadreb/App.kt
+++ app/src/main/java/com/example/computec/eltadreb/App.kt
@@ -10,6 +10,7 @@ import com.example.computec.eltadreb.data.service.Service
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 
 
 class App : Application() {
@@ -17,6 +18,15 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         createApi(null)
+
+        initCalligraphyConfig()
+    }
+
+    private fun initCalligraphyConfig() {
+        CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
+                .setDefaultFontPath(getString(R.string.font_name))
+                .setFontAttrId(R.attr.fontPath)
+                .build())
     }
 
     companion object {
